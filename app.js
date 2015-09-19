6b8276f9c418c3c58e1c57f42b7e5fbb879adf42
theone <theone@theone-ThinkPad-E430c.(none)>
2015-09-19 13:53:40 +0800

    bug match

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(function(req, res){
 
 function saveHtml(url, next){
   var openUrl = config.domain + url,
-  jsAndHtmlCssMatch = /.+(.html)|(\.css)|(\.js)/g;
+  jsAndHtmlCssMatch = /.+(\.html)|(\.css)|(\.js)/g;
 
   if(url.match(jsAndHtmlCssMatch)) {
     return next();
