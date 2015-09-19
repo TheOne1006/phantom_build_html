2134c04d0aa7c8bcf709eb052755d07acda8efbe
theone <theone@theone-ThinkPad-E430c.(none)>
2015-09-19 11:53:39 +0800

    缓存时间

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express'),
 var config = {
   'cachedir' : './_cache',
   'domain' : 'http://www.theone.io',
-  'expiretime' : '3600'
+  'expiretime' : '360000'
 };
 
 // app.get('/', function (req, res) {
