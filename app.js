49c8f4f8a87c0923084c112be13f97b6ee74f10e
theone <297190869@qq.com>
2015-10-26 00:18:08 +0800

    app mod

--- app.js
+++ app.js
@@ -164,11 +164,7 @@ function getFullDir(url, cb) {
  * @return new string 
  */
 function replace_url_by_css (str) {
-<<<<<<< HEAD
   str=str.replace('/public/css/home/',config.domain+'/public/css/home/');
-=======
-  str=str.replace(/\/public\//g,config.domain+"/public/");
->>>>>>> origin/master
   return str;
 }
 
